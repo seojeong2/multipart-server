e55801b4e16290813dd9692dc7547f30747d7aa2
seojeong <seojeong1101@gmail.com>
2023-06-21 18:34:39 +0900

    multipart 테스트 api

--- src/main/java/com/server/multipart/controller/UploadController.java
+++ src/main/java/com/server/multipart/controller/UploadController.java
@@ -2,24 +2,29 @@ package com.server.multipart.controller;
 
 import com.server.multipart.service.UploadFileService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
+@CrossOrigin({"http://localhost:8081", "http://localhost:8083"})
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class UploadController {
 
 
     private final UploadFileService uploadFileService;
 
     @PostMapping("/upload")
-    public ResponseEntity<?> uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
+    public ResponseEntity<?> uploadFile(HttpServletRequest request, @RequestParam(value = "image", required = false) MultipartFile file) throws IOException {
 
+        log.info("call uploadFile");
         uploadFileService.uploadFile(file);
         return ResponseEntity.ok(true);
     }
